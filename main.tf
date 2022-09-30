e10f98074a7e5ad296c532f0215f5ae436c5115e
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-30 12:18:39 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "panos_nat_rule" "nat_rule" {
+  name                  = var.nat_name
+  type                  = var.type
+  description           = var.description
+  source_zones          = var.source_zones
+  destination_zone      = var.destination_zone
+  source_addresses      = var.source_addresses
+  destination_addresses = var.destination_addresses
+  to_interface          = var.to_interface
+  service               = var.service
+  sat_type              = var.sat_type
+  sat_address_type      = var.sat_address_type
+  sat_interface         = var.to_interface
+  sat_ip_address        = var.sat_ip_address
+  dat_type              = var.dat_type
+  dat_address           = var.dat_address
+  dat_port              = var.dat_port
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,80 @@
+variable "type" {
+  type        = string
+  description = "NAT type. This can be ipv4 (default), nat64, or nptv6."
+}
+
+variable "nat_name" {
+  type        = string
+  description = " The NAT rule's name."
+}
+
+variable "description" {
+  type        = string
+  description = "The description of the nat rule"
+}
+
+variable "source_zones" {
+  type        = list(string)
+  description = "List of source zones."
+}
+variable "destination_zone" {
+  type        = string
+  description = "List of source zones."
+}
+
+variable "source_addresses" {
+  type        = list(string)
+  description = "List of source address(es). default is any"
+  default     = ["any"]
+}
+
+variable "destination_addresses" {
+  type        = list(string)
+  description = "List of destination address(es). this is public ip it should be host_ip"
+}
+
+variable "to_interface" {
+  type        = string
+  description = "The interface name. example ethernet1/1"
+}
+
+variable "service" {
+  type        = string
+  description = "The port which should be allowed. default is service-https"
+}
+
+# Source Address Translation
+variable "sat_type" {
+  type        = string
+  description = "Type of source address translation. This can be none (default), dynamic-ip-and-port, dynamic-ip, or static-ip"
+  default     = "dynamic-ip-and-port"
+}
+variable "sat_address_type" {
+  type        = string
+  description = "Source address translation address type. This can be interface-address or translated-address"
+  default     = "interface-address"
+}
+variable "sat_ip_address" {
+  type        = string
+  description = "Source address translation IP address."
+  default     = "None"
+}
+
+# Destination Address Translation
+variable "dat_type" {
+  type        = string
+  description = "Destination address translation type. This should be either static or dynamic. The dynamic option is only available on PAN-OS 8.1+."
+  default     = "static"
+}
+variable "dat_address" {
+  type        = string
+  description = "The private ip"
+}
+variable "dat_port" {
+  type        = number
+  description = "Destination address translation's port number.deafult is 443"
+  default     = 443
+}
+
+
+
